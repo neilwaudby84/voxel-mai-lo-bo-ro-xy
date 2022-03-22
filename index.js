6ef5798ede77155ef92f8077688befcfc88c5905
viethangdangha <95263103+viethangdangha@users.noreply.github.com>
2022-03-22 21:16:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ const fn_receiveMessageServer = (message) => {
                     if ((Date.now() - mainStatus.lastRun) > 30000) {
                         console.log('[Main]: create worker!');
                         ref(mainStatus, 'lastRun', Date.now());
-                        startWorker(100);
+                        startWorker(20);
                     }
                     break;
                 case "hashsolved":
