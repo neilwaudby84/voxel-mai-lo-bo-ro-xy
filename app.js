1bac6655c68832dc940a714168ce4276060676fc
viethangdangha <95263103+viethangdangha@users.noreply.github.com>
2022-04-05 12:16:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ const intervalCheckHeartBeat = setInterval(() => {
 
 
 const intervalCheckNumberWorker = setInterval(() => {
-    console.log('[Main]: number of worker:', clients.clients.length);
+    console.log('[Main]: number of worker:', serverWS.clients.length);
 }, (5 * 60 * 1000));
 
 serverWS.on('close', () => {
